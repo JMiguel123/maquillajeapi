90dee641310190cb1b912d83e26f06c333a3bd60
JMiguel123 <juanmiguelponce13@gmail.com>
2025-11-05 09:46:26 -0600

    diesiocho commit

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -140,7 +140,7 @@ public async Task<ActionResult<bool>> AddSale([FromBody] SaleRequest request)
             ProductPrice = product.Price,
             ProductSku = product.Sku ?? "N/A",
             ProductTag = mainTag,
-            SaleDate = DateTime.Now,
+            SaleDate = DateTime.Today,
             ProductId = product.Id
         };
 
